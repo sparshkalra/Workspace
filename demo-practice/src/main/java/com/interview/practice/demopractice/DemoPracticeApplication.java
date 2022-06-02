6282bac10ed0945e86cf38df742207938ff8ad81
sparshkalra <52778582+sparshkalra@users.noreply.github.com>
2022-06-02 10:59:31 +0530

    Initial commit

---
+++ demo-practice/src/main/java/com/interview/practice/demopractice/DemoPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.interview.practice.demopractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPracticeApplication.class, args);
+	}
+
+}
---
+++ demo-practice/src/test/java/com/interview/practice/demopractice/DemoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.practice.demopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
